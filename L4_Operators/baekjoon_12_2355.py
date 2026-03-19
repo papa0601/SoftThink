4bb54d7215f579349fc13441ba62d4d49939b6f5
papa0601 <rapy0601@naver.com>
2026-03-19 10:31:23 +0900

    complete unsolved one

--- L4_Operators/baekjoon_12_2355.py
+++ L4_Operators/baekjoon_12_2355.py
@@ -1,9 +1,7 @@
 import sys
 input = sys.stdin.readline
 
-# TODO 이거 완성하기 
 a, b = map(int, input().split())
 a, b = (a, b) if a < b else (b, a)
 n = b - a + 1
-avg = (a + b) // 2
-print()
+print(int(n*(n+1)/2 + n*(a-1)))
